d1f9c179a4a8fba2063c127c16e14bee2a148fb3
Anthony Smith <asmithjr1989@gmail.com>
2021-03-30 16:39:51 -0500

    added myList to implements

--- src/com/example/mystructures/queue/BasicArrayQueue.java
+++ src/com/example/mystructures/queue/BasicArrayQueue.java
@@ -1,6 +1,9 @@
 package com.example.mystructures.queue;
 
-public class BasicArrayQueue implements MyQueue {
+import com.example.mystructures.AbstractArrayStructure;
+import com.example.mystructures.lists.MyList;
+
+public class BasicArrayQueue extends AbstractArrayStructure implements MyQueue, MyList  {
     @Override
     public boolean add(Object o) {
         return false;
@@ -24,5 +27,30 @@ public class BasicArrayQueue implements MyQueue {
     @Override
     public Object remove() {
         return null;
+    }
+
+    @Override
+    public boolean add(Object o, int index) {
+        return false;
+    }
+
+    @Override
+    public Object get(int index) {
+        return null;
+    }
+
+    @Override
+    public int getIndex(Object o) {
+        return 0;
+    }
+
+    @Override
+    public boolean remove(int index) {
+        return false;
+    }
+
+    @Override
+    public boolean update(Object o, int index) {
+        return false;
     }
 }
