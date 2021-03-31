403ee020aaf86939c14c23ec9dbc2c44e2bd0dbb
Ben30000 <47725693+Ben30000@users.noreply.github.com>
2021-03-31 04:30:28 -0500

    Updated Set constructor

--- SpecialStructures/src/assignment/Set.java
+++ SpecialStructures/src/assignment/Set.java
@@ -4,7 +4,38 @@ public class Set<T> extends RootStructure<T> {
 
     public Set(T... array ) {
 		//var-args are dynamic, and can accept any number of arguments
-		setStructure(array);
+		//setStructure(array);
+
+		/*
+		 * If an array with duplicates is passed in, remove the extras as per requirements of a Set
+		 */
+    	int uniqueElementCount = 0;
+    	
+		// Null the duplicates of elements in array
+        for (int j = 0; j <= array.length - 1; j++) {
+            for (int i = j + 1; i <= array.length - 1; i++) {
+                if (array[j] == array[i]) {
+                    array[i] = null;
+                }
+            }
+        }
+        // Count how many non null elements remain
+        for (int i = 0; i < array.length; i++) {
+        	if (array[i] != null) {
+        		uniqueElementCount++;
+        	}
+        }
+        // Create a duplicate free array
+        T[] duplicateFreeArray = (T[]) new Object[uniqueElementCount];
+        int duplicateFreeArrayIndex = 0;
+        for (int i = 0; i <= array.length - 1; i++) {
+        	if (array[i] != null) {
+        		duplicateFreeArray[duplicateFreeArrayIndex] = array[i];
+        		duplicateFreeArrayIndex++;
+        	}
+        }
+        
+        setStructure(duplicateFreeArray);
 	}
     
     /**
@@ -12,6 +43,7 @@ public class Set<T> extends RootStructure<T> {
 	 */
     @Override
     public boolean add(T t) {
+    	
             T[] newArray = (T[]) new Object[getStructure().length + 1];
             
             for(int i = 0; i < getStructure().length; i++) {
@@ -39,6 +71,10 @@ public class Set<T> extends RootStructure<T> {
             setStructure(newArray);
             
             return false;
+    }
+    
+    public T get(int i) {
+    	return getStructure()[i];
     }
 
 }
